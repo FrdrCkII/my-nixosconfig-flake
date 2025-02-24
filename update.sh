1f7d24a0f52ad5a8c30571157c56d07d563c759f
FrdrCkII <c2h5oc2h4@outlook.com>
2025-02-25 00:15:55 +0800

    shell

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 sudo rm -rf /etc/nixos/*
 sudo cp -r /home/FrdrCkII/.config/nixos/* /etc/nixos
 sudo rm -rf /etc/nixos/.git
-sudo nixos-rebuild switch --flake /etc/nixos#c2h5oc2h4
+sudo nixos-rebuild switch --impure --flake /etc/nixos#c2h5oc2h4
