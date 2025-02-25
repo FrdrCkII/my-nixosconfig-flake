b7a1c04242bcedfa25fa390637e2aebc253ce7e0
FrdrCkII <c2h5oc2h4@outlook.com>
2025-02-25 20:46:52 +0800

    update

--- modules-home/modules/vscode.nix
+++ modules-home/modules/vscode.nix
@@ -6,9 +6,7 @@
     extensions = with pkgs.vscode-extensions; [
       ms-ceintl.vscode-language-pack-zh-hans
       github.vscode-pull-request-github
-
       jnoortheen.nix-ide
-      tboby.cwtools-vscode
     ];
   };
 }
