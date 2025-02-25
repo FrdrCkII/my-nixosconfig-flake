d2931036acd3ff706b9fb0bb70f0882832d992fd
FrdrCkII <c2h5oc2h4@outlook.com>
2025-02-25 20:46:36 +0800

    update

--- modules-home/modules/vscode.nix
+++ modules-home/modules/vscode.nix
@@ -7,8 +7,6 @@
       ms-ceintl.vscode-language-pack-zh-hans
       github.vscode-pull-request-github
 
-      illixion.vscode-vibrancy-continued
-
       jnoortheen.nix-ide
       tboby.cwtools-vscode
     ];
