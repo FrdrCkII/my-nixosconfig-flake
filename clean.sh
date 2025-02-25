5d5e7ee60ba9736e325ecaa69f6bb7cb1a8803af
FrdrCkII <c2h5oc2h4@outlook.com>
2025-02-25 22:34:18 +0800

    update

--- clean.sh
+++
@@ -1,3 +0,0 @@
-sudo nix profile wipe-history --profile /nix/var/nix/profiles/system
-sudo nix-collect-garbage --delete-old
-nix-collect-garbage --delete-old
--- update.sh
+++
@@ -1,5 +0,0 @@
-sudo nix flake update
-sudo rm -rf /etc/nixos/*
-sudo cp -r /home/FrdrCkII/.config/nixos/* /etc/nixos
-sudo rm -rf /etc/nixos/.git
-sudo nixos-rebuild switch --impure --flake /etc/nixos#c2h5oc2h4
