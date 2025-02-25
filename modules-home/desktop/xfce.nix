706fd781002a764ebe6ef0fb92cc8659f238fa1b
FrdrCkII <c2h5oc2h4@outlook.com>
2025-02-25 21:30:50 +0800

    update

--- modules-home/desktop/xfce.nix
+++ modules-home/desktop/xfce.nix
@@ -1,10 +1,5 @@
 { config, pkgs, lib, configname, ... }:
 
 {
-  home.file = {
-    ".config/xfce4/xfconf" = {
-      source = ../../config-dotfiles/${configname}/xfce4/xfconf;
-      recursive = true;
-    };
-  };
+  
 }
