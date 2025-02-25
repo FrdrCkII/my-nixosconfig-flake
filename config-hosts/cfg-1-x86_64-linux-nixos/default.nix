75575dc908e91d6f681bc052df4ab352fe9accd7
FrdrCkII <c2h5oc2h4@outlook.com>
2025-02-25 18:20:45 +0800

    update

--- config-hosts/cfg-1-x86_64-linux-nixos/default.nix
+++ config-hosts/cfg-1-x86_64-linux-nixos/default.nix
@@ -2,7 +2,7 @@
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
-{ config, lib, pkgs, ... }:
+{ config, lib, pkgs, opt-cfg, ... }:
 
 {
   imports = [
@@ -12,5 +12,8 @@
   system.stateVersion = "25.05";
   system.autoUpgrade.channel = "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/nixos-unstable";
   boot.kernelPackages = pkgs.linuxPackages_zen;
+  users.users.${opt-cfg.username} = {
+    shell = pkgs.zsh;
+  };
 }
 
