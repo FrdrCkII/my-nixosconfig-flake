521c7eb8f435a9d1f308f330ec2e1a0228d6d526
FrdrCkII <c2h5oc2h4@outlook.com>
2025-02-26 19:05:38 +0800

    update

--- modules-system/desktop/hyprland.nix
+++ modules-system/desktop/hyprland.nix
@@ -1,9 +1,9 @@
 { config, lib, pkgs, ... }:
 
 {
-  environment.SystemPackages = with pkgs; {
+  environment.SystemPackages = with pkgs; [
     pulseaudio
-  };
+  ];
   services.displayManager.ly.enable = true;
   services.gnome.gnome-keyring.enable = true;
   programs = {
