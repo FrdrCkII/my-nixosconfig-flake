29dbe3765ba863d10d89e838a4d42a93daa91b09
FrdrCkII <c2h5oc2h4@outlook.com>
2025-02-26 22:55:39 +0800

    update

--- cfg-host/cfg-1-x86_64-linux-nixos/default.nix
+++ cfg-host/cfg-1-x86_64-linux-nixos/default.nix
@@ -19,20 +19,20 @@
     #   "mod-home/modules/zsh.nix"
     # ];
     nixos-modules = [
-      "../../mod-system"
-      "../../mod-system/modules/caddy.nix"
-      "../../mod-system/modules/nix-ld.nix"
-      "../../mod-system/modules/steam.nix"
+      ../../mod-system
+      ../../mod-system/modules/caddy.nix
+      ../../mod-system/modules/nix-ld.nix
+      ../../mod-system/modules/steam.nix
     ];
     home-modules = [
-      "../../mod-home"
-      "../../mod-home/modules/just.nix"
-      "../../mod-home/modules/kitty.nix"
-      "../../mod-home/modules/musicfox.nix"
-      "../../mod-home/modules/ssh.nix"
-      "../../mod-home/modules/vscode.nix"
-      "../../mod-home/modules/yazi.nix"
-      "../../mod-home/modules/zsh.nix"
+      ../../mod-home
+      ../../mod-home/modules/just.nix
+      ../../mod-home/modules/kitty.nix
+      ../../mod-home/modules/musicfox.nix
+      ../../mod-home/modules/ssh.nix
+      ../../mod-home/modules/vscode.nix
+      ../../mod-home/modules/yazi.nix
+      ../../mod-home/modules/zsh.nix
     ];
   };
   options = rec {
