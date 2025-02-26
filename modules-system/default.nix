257eeff230c09a061cf281fad16c43e398a35914
FrdrCkII <c2h5oc2h4@outlook.com>
2025-02-26 12:43:41 +0800

    update

--- modules-system/default.nix
+++ modules-system/default.nix
@@ -18,6 +18,7 @@
     ./modules/caddy.nix
     ./modules/nix-ld.nix
     ./modules/steam.nix
+    ./options.nix
     ./packages.nix
   ]
   ++lib.optionals (builtins.elem "amd" opt-cfg.drivers) [
@@ -45,48 +46,4 @@
     isNormalUser = true;
     extraGroups = [ "wheel" "networkmanager" ];
   };
-  options.opt-cfg = {
-    systemVersion = lib.mkOption {
-      type = lib.types.nullOr lib.types.str;
-      default = "25.05";
-      description = "system state version";
-    };
-    SystemChannel = lib.mkOption {
-      type = lib.types.nullOr lib.types.str;
-      default = null;
-      description = "system channel";
-    };
-    kernelPackages = lib.mkOption {
-      type = lib.types.listOf lib.types.attrs;
-      default = [];
-      description = "kernel packages";
-    };
-    rootpw = lib.mkOption {
-      type = lib.types.nullOr lib.types.str;
-      default = null;
-      description = "root hashed password";
-    };
-    userpw = lib.mkOption {
-      type = lib.types.nullOr lib.types.str;
-      default = null;
-      description = "user hashed password";
-    };
-  };
-  config = lib.mkMerge [
-    (lib.mkIf (opt-cfg.SystemVersion != null) {
-      system.stateVersion = opt-cfg.SystemVersion;
-    })
-    (lib.mkIf (opt-cfg.SystemChannel != null) {
-      system.autoUpgrade.channel = opt-cfg.SystemChannel;
-    })
-    (lib.mkIf (opt-cfg.kernelPackages != []) {
-      boot.kernelPackages = opt-cfg.KernelPackages;
-    })
-    (lib.mkIf (opt-cfg.rootpw != null) {
-      users.users.root.hashedPassword = opt-cfg.rootpw;
-    })
-    (lib.mkIf (opt-cfg.userpw != null) {
-      users.users.${opt-cfg.username}.hashedPassword = opt-cfg.userpw;
-    })
-  ];
 }
---
+++ modules-system/options.nix
@@ -0,0 +1,48 @@
+{ config, lib, pkgs, opt-cfg, ... }:
+
+{
+  options.opt-cfg = {
+    systemVersion = lib.mkOption {
+      type = lib.types.nullOr lib.types.str;
+      default = "25.05";
+      description = "system state version";
+    };
+    SystemChannel = lib.mkOption {
+      type = lib.types.nullOr lib.types.str;
+      default = null;
+      description = "system channel";
+    };
+    kernelPackages = lib.mkOption {
+      type = lib.types.listOf lib.types.attrs;
+      default = [];
+      description = "kernel packages";
+    };
+    rootpw = lib.mkOption {
+      type = lib.types.nullOr lib.types.str;
+      default = null;
+      description = "root hashed password";
+    };
+    userpw = lib.mkOption {
+      type = lib.types.nullOr lib.types.str;
+      default = null;
+      description = "user hashed password";
+    };
+  };
+  config = lib.mkMerge [
+    (lib.mkIf (opt-cfg.SystemVersion != null) {
+      system.stateVersion = opt-cfg.SystemVersion;
+    })
+    (lib.mkIf (opt-cfg.SystemChannel != null) {
+      system.autoUpgrade.channel = opt-cfg.SystemChannel;
+    })
+    (lib.mkIf (opt-cfg.kernelPackages != []) {
+      boot.kernelPackages = opt-cfg.KernelPackages;
+    })
+    (lib.mkIf (opt-cfg.rootpw != null) {
+      users.users.root.hashedPassword = opt-cfg.rootpw;
+    })
+    (lib.mkIf (opt-cfg.userpw != null) {
+      users.users.${opt-cfg.username}.hashedPassword = opt-cfg.userpw;
+    })
+  ];
+}
