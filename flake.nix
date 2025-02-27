8a2a743808919d2425e606fe1fa1ba4e49a33c91
FrdrCkII <c2h5oc2h4@outlook.com>
2025-02-27 15:12:25 +0800

    update

--- flake.nix
+++ flake.nix
@@ -38,5 +38,9 @@
       url = "github:nix-community/flake-firefox-nightly";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    firefox-addons = {
+      url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 }
