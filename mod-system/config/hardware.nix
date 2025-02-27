80dc051bb1bb85cfe3292791d2d7b929297adf6f
FrdrCkII <c2h5oc2h4@outlook.com>
2025-02-27 21:15:04 +0800

    update

--- mod-system/config/hardware.nix
+++ mod-system/config/hardware.nix
@@ -6,10 +6,6 @@
       enable = true;
       enable32Bit = true;
     };
-    i2c = {
-      enable = true;
-      group = "wheel";
-    };
   };
   services.pipewire = {
     enable = true;
--- mod-system/desktop/hyprland.nix
+++ mod-system/desktop/hyprland.nix
@@ -1,26 +1,10 @@
 { config, lib, pkgs, ... }:
 
 {
-  environment.systemPackages = with pkgs; [
-    ddcutil
-  ];
   services.displayManager.ly.enable = true;
   services.gnome.gnome-keyring.enable = true;
   programs = {
     hyprlock.enable = true;
-    hyprland = {
-      enable = true;
-      withUWSM = true;
-    };
-    uwsm = {
-      enable = true;
-      waylandCompositors = {
-        hyprland = {
-          prettyName = "Hyprland";
-          comment = "Hyprland compositor managed by UWSM";
-          binPath = "/run/current-system/sw/bin/Hyprland";
-        };
-      };
-    };
+    hyprland.enable = true;
   };
 }
