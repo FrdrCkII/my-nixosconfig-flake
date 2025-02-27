e92bd1fd738e9d45c7c7473df97d796258657b72
FrdrCkII <c2h5oc2h4@outlook.com>
2025-02-27 14:41:11 +0800

    update

--- mod-home/modules/firefox.nix
+++ mod-home/modules/firefox.nix
@@ -1,9 +1,13 @@
-{ inputs, config, pkgs, lib, cfg, ... }:
-
-{
+{ config, pkgs, lib, cfg, ... }: let
+  mozilla-overlays = fetchTarball {
+      url = https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz;
+  };
+in {
+  nixpkgs.overlays = [ (import "${mozilla-overlays}") ];
+  xdg.configFile."nixpkgs/config.nix".source = mozilla-overlays;
   programs.firefox = {
     enable = true;
     languagePacks = [ "zh-CN" ];
-    package = inputs.firefox.packages.${pkgs.system}.firefox-nightly-bin;
+    package = pkgs.latest.firefox-nightly-bin;
   };
 }
