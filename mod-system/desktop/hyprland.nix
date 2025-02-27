3c4faf012f324332706b04fc2f7ccc5b82ce0190
FrdrCkII <c2h5oc2h4@outlook.com>
2025-02-27 19:20:01 +0800

    update

--- mod-system/desktop/hyprland.nix
+++ mod-system/desktop/hyprland.nix
@@ -1,9 +1,9 @@
 { config, lib, pkgs, ... }:
 
 {
-  environment.systemPackages = with pkgs; {
+  environment.systemPackages = with pkgs; [
     brightnessctl
-  };
+  ]
   services.displayManager.ly.enable = true;
   services.gnome.gnome-keyring.enable = true;
   programs = {
