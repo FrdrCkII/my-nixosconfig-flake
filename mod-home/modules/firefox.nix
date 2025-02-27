f5e36ae1c4e8b457b13acc08f3fd0945776901dd
FrdrCkII <c2h5oc2h4@outlook.com>
2025-02-27 16:55:29 +0800

    update

--- mod-home/modules/firefox.nix
+++ mod-home/modules/firefox.nix
@@ -26,15 +26,6 @@
         packages = with pkgs.nur.repos.rycee.firefox-addons; [
           ublock-origin
         ];
-        settings."uBlock0@raymondhill.net".settings = {
-          selectedFilterLists = [
-            "ublock-filters"
-            "ublock-badware"
-            "ublock-privacy"
-            "ublock-unbreak"
-            "ublock-quick-fixes"
-          ];
-        };
       };
       search = {
         default = "CNBing";
