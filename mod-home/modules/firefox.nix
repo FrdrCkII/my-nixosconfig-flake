392390f562fb9650080fc579409ff231ef90f196
FrdrCkII <c2h5oc2h4@outlook.com>
2025-02-27 17:05:40 +0800

    update

--- mod-home/modules/firefox.nix
+++ mod-home/modules/firefox.nix
@@ -24,7 +24,7 @@
       };
       extensions = {
         packages = with pkgs.nur.repos.rycee.firefox-addons; [
-          ublock-origin
+          localeCDN
         ];
       };
       search = {
