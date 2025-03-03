7a2e81c304c1bb26c7e2091ceb8b51fa64b2f6f0
FrdrCkII <c2h5oc2h4@outlook.com>
2025-03-03 18:06:16 +0800

    update

--- cfg-host/cfg-1-x86_64-linux-nixos/modules.nix
+++ cfg-host/cfg-1-x86_64-linux-nixos/modules.nix
@@ -5,8 +5,8 @@
     nixos-modules = [
       ./system
       ../../mod-system
-      # ../../mod-system/config/boot.nix
-      ../../mod-system/config/boot.systemd-boot.nix
+      ../../mod-system/config/boot.nix
+      # ../../mod-system/config/boot.systemd-boot.nix
       ../../mod-system/config/hardware.nix
       ../../mod-system/config/i18n.nix
       ../../mod-system/config/nix.nix
