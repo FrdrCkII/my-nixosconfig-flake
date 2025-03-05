44f3e4e03f493bbd7d7eb07b0a606902488119f6
FrdrCkII <c2h5oc2h4@outlook.com>
2025-03-05 19:38:55 +0800

    update

--- cfg-host/cfg-2-x86_64-linux-arch/options.nix
+++ cfg-host/cfg-2-x86_64-linux-arch/options.nix
@@ -2,6 +2,7 @@
 
 {
   options = rec {
+    is-other-distro = true;
     username = "FrdrCkII";
     # rootpw = "$y$j9T$YHvpqmryW6Uk4LsBPj3S41$bAMv6EQYDOrQ3kAagjf.2TPFndEAuEjllKFeFrBlfM9";
     # userpw = "$y$j9T$fenbjjJWwGfICJPdwhI561$3Aiwlijs42HSUNPptXm444QDxBrWI9rPwFrOmqcqo2.";
