02d333910f3b34725235825e9aa60f01b08d7eaf
FrdrCkII <c2h5oc2h4@outlook.com>
2025-03-05 19:57:05 +0800

    update

--- cfg-host/cfg-2-x86_64-linux-arch/modules.nix
+++ cfg-host/cfg-2-x86_64-linux-arch/modules.nix
@@ -4,13 +4,13 @@
   modules = rec {
     home-modules = [
       ../../mod-home/standalone.nix
-      ../../mod-home/modules/aria2.nix
-      ../../mod-home/modules/just.nix
-      ../../mod-home/modules/kitty.nix
-      ../../mod-home/modules/musicfox.nix
-      ../../mod-home/modules/ssh.nix
-      ../../mod-home/modules/yazi.nix
-      ../../mod-home/modules/zsh.nix
+      # ../../mod-home/modules/aria2.nix
+      # ../../mod-home/modules/just.nix
+      # ../../mod-home/modules/kitty.nix
+      # ../../mod-home/modules/musicfox.nix
+      # ../../mod-home/modules/ssh.nix
+      # ../../mod-home/modules/yazi.nix
+      # ../../mod-home/modules/zsh.nix
     ];
   };
 }
