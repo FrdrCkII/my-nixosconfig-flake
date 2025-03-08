96f7b10e27306a22e5d7e3dda88fa00452f6cb69
FrdrCkII <c2h5oc2h4@outlook.com>
2025-03-08 22:52:46 +0800

    update

--- mod-standalone-home/modules/aria2.nix
+++ mod-standalone-home/modules/aria2.nix
@@ -7,4 +7,17 @@
       recursive = true;
     };
   };
+  systemd.user.services.aria2cd = {
+    Unit = {
+      Description = "Aria2 Daemon";
+    };
+
+    Service = {
+      ExecStart = "/usr/bin/aria2c --conf-path=/home/${cfg.opt.username}/.aria2/aria2.conf";
+    };
+
+    Install = {
+      WantedBy = [ "default.target" ];
+    };
+  };
 }
