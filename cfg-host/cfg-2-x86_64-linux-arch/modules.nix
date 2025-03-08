4c331b9594906c867b7e43147d851ef8aa8ded55
FrdrCkII <c2h5oc2h4@outlook.com>
2025-03-08 14:59:04 +0800

    update

--- cfg-host/cfg-2-x86_64-linux-arch/modules.nix
+++ cfg-host/cfg-2-x86_64-linux-arch/modules.nix
@@ -11,6 +11,7 @@
       ../../mod-standalone-home
       ../../mod-standalone-home/modules/aria2.nix
       ../../mod-standalone-home/modules/ghostty.nix
+      ../../mod-standalone-home/modules/ghostty.tty1.nix
       ../../mod-standalone-home/modules/musicfox.nix
       ../../mod-standalone-home/modules/ssh.nix
       ../../mod-standalone-home/modules/yazi.nix
