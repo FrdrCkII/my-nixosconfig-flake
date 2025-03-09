055274049515e59eaa54eccbc9ad793cbb27fe49
FrdrCkII <c2h5oc2h4@outlook.com>
2025-03-09 13:30:07 +0800

    update

--- mod-standalone-home/drivers/amd.nix
+++ mod-standalone-home/drivers/amd.nix
@@ -4,15 +4,18 @@
   home.packages = with pkgs; [
     lact
   ];
-  # systemd.user.services.lactd = {
-  #   Unit = {
-  #     Description = "LACT Daemon";
-  #   };
-  #   Service = {
-  #     ExecStart = "";
-  #   };
-  #   Install = {
-  #     WantedBy = [ "default.target" ];
-  #   };
-  # };
+  systemd.user.services.lactd = {
+    Unit = {
+      Description = "AMDGPU Control Daemon";
+      After = [ "multi-user.target" ];
+    };
+    Service = {
+      ExecStart = "lact daemon";
+      Nice = "-10";
+      Restart = "on-failure";
+    };
+    Install = {
+      WantedBy = [ "multi-user.target" ];
+    };
+  };
 }
