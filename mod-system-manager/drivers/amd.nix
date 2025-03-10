11616103c6c96f1a7ca509e45b961a3ba6d71354
FrdrCkII <c2h5oc2h4@outlook.com>
2025-03-10 14:44:18 +0800

    update

--- mod-system-manager/drivers/amd.nix
+++ mod-system-manager/drivers/amd.nix
@@ -1,27 +1,27 @@
 { config, lib, pkgs, ... }:
 
 {
-  config = {
-    environment.systemPackages = with pkgs; [
-      lact
-    ];
-    # systemd.services = {
-    #   lactd = {
-    #     enable = true;
-    #     unitConfig = {
-    #       Description = "AMDGPU Control Daemon";
-    #       After = [ "multi-user.target" ];
-    #     };
-    #     serviceConfig = {
-    #       Environment = "";
-    #       ExecStart = "lact daemon";
-    #       Nice = "-10";
-    #       Restart = "on-failure";
-    #     };
-    #     wantedBy = [ "multi-user.target" ];
-    #   };
-    # };
-    systemd.packages = with pkgs; [ lact ];
-    systemd.services.lactd.wantedBy = ["multi-user.target"];
-  };
+  # config = {
+  #   environment.systemPackages = with pkgs; [
+  #     lact
+  #   ];
+  #   # systemd.services = {
+  #   #   lactd = {
+  #   #     enable = true;
+  #   #     unitConfig = {
+  #   #       Description = "AMDGPU Control Daemon";
+  #   #       After = [ "multi-user.target" ];
+  #   #     };
+  #   #     serviceConfig = {
+  #   #       Environment = "";
+  #   #       ExecStart = "lact daemon";
+  #   #       Nice = "-10";
+  #   #       Restart = "on-failure";
+  #   #     };
+  #   #     wantedBy = [ "multi-user.target" ];
+  #   #   };
+  #   # };
+  #   systemd.packages = with pkgs; [ lact ];
+  #   systemd.services.lactd.wantedBy = ["multi-user.target"];
+  # };
 }
