81cbd27bef67dfd19f79bdb3c391d96bb0a9a76c
FrdrCkII <c2h5oc2h4@outlook.com>
2025-03-10 14:42:59 +0800

    update

--- mod-system-manager/drivers/amd.nix
+++ mod-system-manager/drivers/amd.nix
@@ -5,21 +5,23 @@
     environment.systemPackages = with pkgs; [
       lact
     ];
-    systemd.services = {
-      lactd = {
-        enable = true;
-        unitConfig = {
-          Description = "AMDGPU Control Daemon";
-          After = [ "multi-user.target" ];
-        };
-        serviceConfig = {
-          Environment = "";
-          ExecStart = "lact daemon";
-          Nice = "-10";
-          Restart = "on-failure";
-        };
-        wantedBy = [ "multi-user.target" ];
-      };
-    };
+    # systemd.services = {
+    #   lactd = {
+    #     enable = true;
+    #     unitConfig = {
+    #       Description = "AMDGPU Control Daemon";
+    #       After = [ "multi-user.target" ];
+    #     };
+    #     serviceConfig = {
+    #       Environment = "";
+    #       ExecStart = "lact daemon";
+    #       Nice = "-10";
+    #       Restart = "on-failure";
+    #     };
+    #     wantedBy = [ "multi-user.target" ];
+    #   };
+    # };
+    systemd.packages = with pkgs; [ lact ];
+    systemd.services.lactd.wantedBy = ["multi-user.target"];
   };
 }
